4bdc9de534db1a0d3c229b570b1bb3ae5b1c3d5e
yangcl <210485393@qq.com>
2018-05-15 17:37:02 +0800

    add ele project

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <transition name="router-fade" mode="out-in">
+      <keep-alive>
+        <router-view v-if="$route.meta.keepAlive"></router-view>
+      </keep-alive>
+    </transition>
+    <transition name="router-fade" mode="out-in">
+      <router-view v-if="!$route.meta.keepAlive"></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="scss">
+  @import './style/common';
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  .router-fade-enter-action, .router-fade-leave-action {
+    -webkit-transition: opacity .3s;
+    -moz-transition: opacity .3s;
+    -ms-transition: opacity .3s;
+    -o-transition: opacity .3s;
+    transition: opacity .3s;
+  }
+  .router-fade-enter, .router-fade-leave-action {
+    opacity: 0;
+  }
+</style>
---
+++ src/service/getData.js
@@ -0,0 +1,48 @@
+import fetch from '../config/fetch';
+import {getStore} from '../config/mUtils'
+/*获取首页默认地址
+* */
+export const cityGuess = () => fetch('/v1/cities', {
+  type: 'guess'
+});
+//获取首页热门城市
+export const hotcity = () => fetch('/v1/cities', {
+  type: 'hot'
+});
+//获取首页所有城市
+export const groupcity = () => fetch('/v1/cities', {
+  type: 'group'
+});
+//获取当前所在城市
+export const currentcity = number => fetch('/v1/cities/' + number);
+//获取搜素地址
+export const searchplace = (cityid, value) => fetch('/v1/pois', {
+  type: 'search',
+  city_id: cityid,
+  keyword: value
+})
+//获取msite页面地址信息
+export const msiteAddress = geohash => fetch('/v2/pois/' + geohash);
+//获取msite页面食品分类列表
+export const msiteFoodTypes = geohash => fetch('v2/index_entry', {
+  geohash,
+  group_type: '1',
+  'flags[]': 'F'
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
